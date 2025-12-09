e528f8ea189d3b37221ed7b0ae064b77d5298ec0
Luara Amaral <luaraamaral.ti@gmail.com>
2025-12-09 11:07:45 -0300

    feat: integration feign client

--- src/main/java/com/latech/register/RegisterApplication.java
+++ src/main/java/com/latech/register/RegisterApplication.java
@@ -2,7 +2,9 @@ package com.latech.register;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class RegisterApplication {
 
